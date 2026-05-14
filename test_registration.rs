306552ecf877d8894ce7fb75e180978c9ab99f29
Gabriel Maeztu <gabriel.maeztu@gmail.com>
2026-05-14 09:08:44 +0200

    chore: remove LLM helper files wrongly commmited to the repo
    Signed-off-by: Gabriel de Maeztu <gabriel.maeztu@gmail.com>

--- test_registration.rs
+++
@@ -1,6 +0,0 @@
-// Copyright 2025 Oxibase Contributors
-fn main() {
-    let registry = oxibase::functions::registry::global_registry();
-    println!("Supported languages:");
-    // We can't access user_defined_functions directly if it's private, but we can try something
-}
