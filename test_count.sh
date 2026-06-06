3cace60866273de1971975d33cf8d6a49f4c0c79
Gabriel Maeztu <gabriel.maeztu@gmail.com>
2026-06-07 00:04:18 +0200

    chore: remove untracked test files

--- test_count.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cargo run -- repl -d memory:// << 'SQL'
-CREATE TABLE test (id INT);
-INSERT INTO test VALUES (1);
-INSERT INTO test VALUES (2);
-SELECT COUNT(*) FROM test;
-SELECT COUNT(id) FROM test;
-SQL
